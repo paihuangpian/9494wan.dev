12377026e0dc64012a41cb46ea07605d22d79c35
paihuangpian <2451678579@qq.com>
2016-03-29 17:56:31 +0800

    '去掉了模版引擎tempo.jsjs'

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,28 +4,13 @@
         <meta charset="utf-8">
         <title>Laravel 后台管理系统</title>
         <link href="http://cdn.bootcss.com/normalize/3.0.3/normalize.min.css" rel="stylesheet">
-        <link href="/css/laravel.css" rel="stylesheet">
-        <script src="/js/tempo.min.js" charset="utf-8"></script>
-        <script src="http://cdn.bootcss.com/jquery/3.0.0-alpha1/jquery.min.js"></script>
-        <script type="text/javascript">
-            var url = "{{ url('nav') }}";
-            $(document).ready(function () {
-                $.getJSON(url, function(data) {
-                    Tempo.prepare("menu").render(data);
-                });
-            });
-        </script>
-        <style media="screen">
-            .loading{
-                background: red;
-            }
-        </style>
+        <link href="/css/admin.css" rel="stylesheet">
     </head>
     <body>
         <div class="nav">
             <a href="javascript:;" class="brand">Laravel</a>
-            <ul id="menu" data-template>
-                <li><a href="">@{{ name }}</a></li>
+            <ul>
+                <li><a href="" class="active">仪表盘</a></li>
             </ul>
         </div>
         <div class="sidebar">
@@ -35,7 +20,7 @@
             <a href="">数据库</a>
         </div>
         <div class="container">
-            <div class="title"><a href="#" class="active">所有管理员</a> <span>/</span> <a href="#">激活用户</a></div>
+            <div class="title"><a href="#" class="active">所有管理员<small>(98)</small></a> <span>/</span> <a href="#">激活用户<small>(78)</small></a></div>
             <div class="action">
                 全选：
                 <select class="" name="">
