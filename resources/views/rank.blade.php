699a93f91a9de4f84bdddbfcc5f130fdc5e95ee6
paihuangpian <2451678579@qq.com>
2016-04-28 18:00:29 +0800

    '排行冠亚军'

--- resources/views/rank.blade.php
+++ resources/views/rank.blade.php
@@ -4,9 +4,10 @@
 <div style="width: 49%;float: left;">
     <div style="height: 50px;line-height: 50px;text-align: center;">英雄榜</div>
     <table>
-        <tr><th>军徽</th><th>英雄</th><th>总战功</th></tr>
-        @foreach($persons as $person)
+        <tr><th>榜</th><th>军徽</th><th>英雄</th><th>总战功</th></tr>
+        @foreach($persons as $key => $person)
         <tr>
+            <td>@if(($key + 1) == 1) 冠军 @elseif(($key + 1) == 2) 亚军 @elseif(($key + 1) == 3) 季军 @endif</td>
             <td><img src="/images/sign/{{ \DB::table('levels')->find(\DB::table('user_levels')->where('user_id', Auth::user()->id)->orderBy('id', 'desc')->first()->level_id)->sign }}" width="30"></td>
             <td>{{ $person->name }} </td>
             <td>{{ $person->experience }}</td>
