5a129254aa8e7ab956c8b2c408435197706074c6
paihuangpian <2451678579@qq.com>
2016-06-01 18:28:42 +0800

    '完成'

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -146,7 +146,7 @@ Route::group(['middleware' => 'web'], function () {
             $yesterday_groups = \DB::select("select *, sum(recharge) as total from records where created_at = '" . $yesterday_time . "' group by group_id order by total desc");
 
             $last_month_persons = \DB::select("select *, sum(recharge) as total from records where date_format(created_at,'%Y-%m')=date_format(DATE_SUB(curdate(), INTERVAL 1 MONTH),'%Y-%m') group by user_id order by total desc limit 0, 10");
-            $last_month_groups = \DB::select("select *, sum(recharge) as total from records where date_format(created_at,'%Y-%m')=date_format(DATE_SUB(curdate(), INTERVAL 1 MONTH),'%Y-%m') group by group_id order by total");
+            $last_month_groups = \DB::select("select *, sum(recharge) as total from records where date_format(created_at,'%Y-%m')=date_format(DATE_SUB(curdate(), INTERVAL 1 MONTH),'%Y-%m') group by group_id order by total desc");
             
             $todays = \DB::select("select *, sum(recharge) as total from records where created_at = '" . date('Y-m-d', time()) . "' group by user_id order by total desc limit 0, 10");
             $today_groups = \DB::select("select *, sum(recharge) as total from records where created_at = '" . date('Y-m-d', time()) . "' group by group_id order by total desc");
