1dfe4207ac378a89a43147b400cf869625911b5d
paihuangpian <2451678579@qq.com>
2016-06-01 18:24:38 +0800

    '搞定'

--- resources/views/rank.blade.php
+++ resources/views/rank.blade.php
@@ -91,11 +91,15 @@
     <table>
         <tr><th>榜</th><th>军团</th><th>总战功</th></tr>
         @foreach($last_month_groups as $key => $last_month_group)
-        <tr>
-            <td>@if(($key + 1) == 1) <span style="color:#eead0e">冠军</span> @elseif(($key + 1) == 2) <span style="color:#cdc1c5">亚军</span> @elseif(($key + 1) == 3) <span style="color:#cd950c">季军</span> @else  {{ $key + 1 }}  @endif</td>
-            <td>@if($last_month_group->group_id) @if(\DB::table('groups')->find($last_month_group->group_id)) {{ \DB::table('groups')->find($last_month_group->group_id)->name }} @endif @else 未知 @endif</td>
-            <td>{{ $last_month_group->total }}</td>
-        </tr>
+            @if($last_month_group->group_id)
+                @if(\DB::table('groups')->find($last_month_group->group_id))
+                    <tr>
+                        <td>@if(($key + 1) == 1) <span style="color:#eead0e">冠军</span> @elseif(($key + 1) == 2) <span style="color:#cdc1c5">亚军</span> @elseif(($key + 1) == 3) <span style="color:#cd950c">季军</span> @else  {{ $key + 1 }}  @endif</td>
+                        <td>{{ \DB::table('groups')->find($last_month_group->group_id)->name }}</td>
+                        <td>{{ $last_month_group->total }}</td>
+                    </tr>
+                @endif
+            @endif
         @endforeach
     </table>
 </div>
